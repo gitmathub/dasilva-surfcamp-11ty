486b285bb826093beab777e9778493959801cc95
gitmatlab <mat@alabaster.de>
2020-12-05 12:43:41 +0100

    positioned logo

--- src/data/links.js
+++ src/data/links.js
@@ -8,16 +8,56 @@ module.exports = {
     bedAndBreakfast: {
       title: "Unterkunft",
       path: "/de/unterkunft"
-    }    
+    },
+    mountainbikes: {
+      title: "Moutainbikes",
+      path: "/de/mountainbikes"
+    },
+    kidsAreWelcome: {
+      title: "Kids are welcome",
+      path: "/de/kidsAreWelcome"
+    },
+    happyHorses: {
+      title: "Happy Horses",
+      path: "/de/happyHorses"
+    },
+    activities: {
+      title: "Aktivitäten",
+      path: "/de/aktivitaeten"
+    },
+    booking: {
+      title: "Booking",
+      path: "/de/booking"
+    }
   },
   en: {
     home: {
       title: "Home",
       path: "/en"
-    },    
+    },
     bedAndBreakfast: {
       title: "Bed and Breakfast",
       path: "/en/bed-and-breakfast"
+    },
+    mountainbikes: {
+      title: "Moutainbikes",
+      path: "/de/mountainbikes"
+    },
+    kidsAreWelcome: {
+      title: "Kids are welcome",
+      path: "/de/kidsAreWelcome"
+    },
+    happyHorses: {
+      title: "Happy Horses",
+      path: "/de/happyHorses"
+    },
+    activities: {
+      title: "Aktivitäten",
+      path: "/de/aktivitaeten"
+    },
+    booking: {
+      title: "Booking",
+      path: "/de/booking"
     }    
   }
 }
