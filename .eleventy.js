21ef330bd8aa083627bb060b2f9516397147fad5
gitmatlab <mat@alabaster.de>
2021-01-24 21:49:39 +0100

    generated image deployment

--- .eleventy.js
+++ .eleventy.js
@@ -2,92 +2,80 @@ const pluginSass = require("eleventy-plugin-sass")
 const Image = require("@11ty/eleventy-img")
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({
-        "src/assets": "_assets/",
-        "img": "/_assets/images/"
+
+  eleventyConfig.addPassthroughCopy({
+    "src/assets": "_assets/",
+    "generated-images": "_assets/images",
+  })
+
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: ["src/styles/*.scss"],
+    outputDir: "dist/_assets"
+  })
+
+
+  // override anouying md code formatting
+  const markdownIt = require("markdown-it")({
+      html: true
     })
-    eleventyConfig.addPlugin(pluginSass, {
-        watch: ["src/styles/*.scss"],
-        outputDir: "dist/_assets"
-    })
+    .disable('code')
+  // .use(require('markdown-it-github-headings'))
+  eleventyConfig.setLibrary("md", markdownIt)
 
-    // override anouying md code formatting
-    const options = {
-        html: true
-    }
-    const markdownIt = require("markdown-it")(options)
-        .disable('code')
-    // .use(require('markdown-it-github-headings'))
-    eleventyConfig.setLibrary("md", markdownIt)
+  // generate images
+  eleventyConfig.addNunjucksAsyncShortcode("image", ({
+    src,
+    alt,
+    myClass
+  }) => generateImages({
+    src,
+    alt,
+    myClass
+  }))
 
-    // PICTURE
-    eleventyConfig.addNunjucksAsyncShortcode("responsiveimage", async function (src, alt, sizes = "100vw") {
-        // console.log("parameters: ", src, alt, sizes)
-        if (alt === undefined) {
-            throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-        }
+  return {
+    dir: {
+      input: 'src/pages',
+      layouts: "../templates",
+      includes: "../templates",
+      data: "../data",
+      output: 'dist'
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['njk', 'md', 'css', 'html'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk"
+  }
+}
 
-        let metadata = await Image(src, {
-            widths: [230, 300, 350, 480, 768, 1024],
-            formats: ['webp', 'jpeg'],
-            urlPath: "/_assets/images/",
-            outputDir: "src/assets/images",
-        });
-        let lowsrc = metadata.jpeg[0];
-        // console.log("lowsrc", lowsrc)
 
-        return `<picture>
-      ${Object.values(metadata).map(imageFormat => {
-          console.log("sizes", sizes)
-        return `  <source type="image/${imageFormat[0].format}" 
-        srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-      }).join("\n")}
-        <img
-          src="${lowsrc.url}"
-          width="${lowsrc.width}"
-          height="${lowsrc.height}"
-          alt="${alt}">
-      </picture>`;
-    })
+async function generateImages({
+  src,
+  alt,
+  myClass
+}) {
 
-    // IMAGE
-    eleventyConfig.addNunjucksAsyncShortcode("image", async ({
-        src,
-        alt,
-        myClass
-    }) => {
-        if (alt === undefined) {
-            throw new Error(`Missing \`alt\` on myImage from: ${src}`);
-        }
-        const filename = src.match(/([^\/]+)\.(\w+)$/)[1]
-        const metadata = await Image(src, {
-            widths: [230, 300, 350, 480, 768, 1024, 2048],
-            formats: ['jpeg'], //'webp'
-            urlPath: "/_assets/images/",
-            outputDir: "img",
-            filenameFormat: (id, src, width, format, options) => `${filename}-${width}-${id}.${format}`
-        });
-        const srcset = metadata.jpeg.filter(i => i).map(i => `${i.url} ${i.width}w`).join(', ')
-        return `<img alt="${alt}" 
+  if (alt === undefined) {
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  const filename = src.match(/([^\/]+)\.(\w+)$/)[1]
+
+  const metadata = await Image(src, {
+    widths: [230, 300, 350, 480, 768, 1024, 2048],
+    formats: ['jpeg'], //'webp'
+    urlPath: "/_assets/images/",
+    outputDir: "generated-images",
+    filenameFormat: (id, src, width, format, options) => `${filename}-${width}-${id}.${format}`
+  });
+
+  const srcset = metadata.jpeg.filter(i => i).map(i => `${i.url} ${i.width}w`).join(', ')
+
+  return `<img alt="${alt}" 
             data-sizes="auto"
             data-srcset="${srcset}"
             class="lazyload ${myClass}"
             style="display:block"
             >`;
-        // style="display:block is important for lazyload to calculate correctly
-    });
-
-    return {
-        dir: {
-            input: 'src/pages',
-            layouts: "../templates",
-            includes: "../templates",
-            data: "../data",
-            output: 'dist'
-        },
-        passthroughFileCopy: true,
-        templateFormats: ['njk', 'md', 'css', 'html'],
-        htmlTemplateEngine: 'njk',
-        markdownTemplateEngine: "njk"
-    }
+  // style="display:block is important for lazyload to calculate correctly
 }
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,9 +4,6 @@ pwd
 echo 'copy htaccess'
 cp -v config/htaccess dist/.htaccess
 
-echo 'copy generated images'
-cp -va img dist/
-
 echo "rename dist folder"
 new_folder="dasilva-`date +%Y%m%d%H%M`"
 mv -v dist $new_folder
